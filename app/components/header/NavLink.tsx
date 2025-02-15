814225b59de5f35996d2f63f2406136550f9ba6e
Venczel Patrik <v.papatrik@gmail.com>
2025-02-15 22:49:05 +0100

    feat: banner links and label

--- app/components/header/NavLink.tsx
+++ app/components/header/NavLink.tsx
@@ -1,14 +1,20 @@
+import React from "react";
+
+interface NavLinkProps {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
 export default function NavLink({
   href,
   children,
-}: {
-  href: string;
-  children: React.ReactNode;
-}) {
+  className = "",
+}: NavLinkProps) {
   return (
     <a
       href={href}
-      className="text-white hover:underline transition-colors text-sm/6 font-semibold"
+      className={`text-white hover:underline transition-colors text-sm/6 font-semibold ${className}`}
     >
       {children}
     </a>
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,86 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import NavLink from "./components/header/NavLink";
 
 export default function Home() {
   return (
     <div>
       <main>
         <div className="w-full flex lg:flex-row flex-col gap-0.5">
-          <div className="relative lg:w-1/4 h-screen">
-            <div className="absolute inset-0 bg-black opacity-50"></div>
-            <div className="bg-[url(/img/gitar.jpg)] bg-cover h-full bg-[30%]"></div>
-          </div>
-          <div className="order-first lg:order-none h-screen lg:w-2/4">
-            <div className="relative lg:h-1/2 lg:mb-0.5">
-              <div className="absolute inset-0 bg-black opacity-50"></div>
-              <div className="bg-[url(/img/grund2.png)] bg-cover h-full"></div>
+          <div className="relative h-[50vh] lg:w-1/4  lg:h-screen">
+            <div className="relative bg-[url(/img/gitar.jpg)] bg-cover h-full bg-[30%] group">
+              <div className="absolute inset-0 bg-black opacity-40"></div>
+              <NavLink
+                href="zenes"
+                className="relative z-10 text-white top-3/5 transform -translate-y-1/2 ml-5 block lg:opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+              >
+                <h2 className="text-white opacity-100 underline">Zenés</h2>
+                <h2 className="ml-5 opacity-40">Coaching</h2>
+              </NavLink>
+              <NavLink
+                href="verses"
+                className="relative z-10 text-white top-4/5 transform -translate-y-1/2 text-right mr-5 block lg:opacity-0  group-hover:opacity-100 transition-opacity duration-500"
+              >
+                <h2 className="text-white opacity-100 mr-12 underline">
+                  Verses
+                </h2>
+                <h2 className=" opacity-40">Coaching</h2>
+              </NavLink>
             </div>
-            <div className="relative bg-[url(/img/reten.jpg)] bg-cover h-screen lg:h-1/2 lg:bg-bottom order-first lg:order-none flex items-center justify-center">
-              <div className="bg-black/50 text-white p-6 rounded-xl shadow-lg max-w-2xl text-center">
-                <h2 className="text-2xl lg:text-4xl font-bold">
-                  Kreatív coaching eszközökkel a korlátaidon túlra...
+          </div>
+          <div className="order-first lg:order-none lg:w-2/4 flex flex-col">
+            <div className="relative h-[50vh] lg:h-1/2 lg:mb-0.5 group  mt-0.5 lg:mt-0">
+              <div className="absolute inset-0 bg-black opacity-50"></div>
+              <div className="bg-[url(/img/grund2.png)] bg-cover h-full flex items-end justify-between pb-8 px-3">
+                <NavLink
+                  href="tarsas"
+                  className="relative z-10 text-white ml-5 block mb-16 lg:opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+                >
+                  <h2 className="text-white opacity-100 underline">
+                    Társasjáték
+                  </h2>
+                  <h2 className="ml-5 opacity-40">Coaching</h2>
+                </NavLink>
+                <NavLink
+                  href="d20"
+                  className="relative z-10 text-white text-right mr-5 block lg:opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+                >
+                  <h2 className="text-white opacity-100 mr-4 underline">
+                    Szerepjáték
+                  </h2>
+                  <h2 className=" opacity-40">Coaching</h2>
+                </NavLink>
+              </div>
+            </div>
+            <div className="relative bg-[url(/img/reten.jpg)] bg-cover h-screen lg:h-1/2 lg:bg-bottom order-first lg:order-none flex items-end justify-end group">
+              <div className="bg-black/40 text-white p-6 shadow-lg text-center mb-15">
+                <h2 className="text-2xl lg:text-2xl text-right italic">
+                  Kreatív coaching eszközökkel
+                  <br /> a korlátokon túlra...
                 </h2>
               </div>
             </div>
           </div>
-          <div className="relative lg:w-1/4 h-screen">
+          <div className="relative lg:w-1/4 h-[50vh] lg:h-screen group">
             <div className="absolute inset-0 bg-black opacity-50"></div>
-            <div className="bg-[url(/img/sup.jpg)] bg-cover h-full bg-[30%]"></div>
+            <div className="bg-[url(/img/sup.jpg)] bg-cover h-full bg-[30%]">
+              <NavLink
+                href="szabadban"
+                className="relative z-10 text-white top-3/5 transform -translate-y-1/2 ml-5 block lg:opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+              >
+                <h2 className="text-white opacity-100 underline">Szabadban</h2>
+                <h2 className="ml-5 opacity-40">Coaching</h2>
+              </NavLink>
+              <NavLink
+                href="onthesup"
+                className="relative z-10 text-white top-4/5 transform -translate-y-1/2 text-right mr-5 block lg:opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+              >
+                <h2 className="text-white opacity-100 mr-12 underline">
+                  On the SUP
+                </h2>
+                <h2 className=" opacity-40">Coaching</h2>
+              </NavLink>
+            </div>
           </div>
         </div>
 
