d8c1772d90793e94b4a719fb348e26f9997ece0a
Venczel Patrik <v.papatrik@gmail.com>
2025-06-01 11:45:03 +0200

    add onTheSup page content

--- app/onthesup/page.tsx
+++ app/onthesup/page.tsx
@@ -1,8 +1,42 @@
-export default function Rolam() {
+import SideboxrBanner from "../components/SideboxBanner";
+
+export default function OnTheSup() {
   return (
     <div>
       <main>
-        <div className="bg-[url(/img/reten.jpg)] bg-cover h-screen w-full"></div>
+        <SideboxrBanner
+          imageUrl="/img/sup.webp"
+          title="On The Sup coaching"
+          subtitle="5 alkalmas segítő beszélgetés munkahelyi és személyes életbeli elakadások segítésére, a vizen SUP-al lebegve"
+        >
+          <p>
+            A szabadtéri coaching egy extrémebb változata, ahol a másfél órás
+            coaching alkalmat egy <strong>25 perces evezéssel kezdjük.</strong>
+          </p>
+
+          <p>
+            A természet közelsége és a mozgás segítenek fejben megérkezni, hogy
+            azután a<strong> víztükrön lebegve</strong>, a supon egymás felé
+            fordulva, a hagyományos kereteket tartva egy minőségi coaching
+            alkalom jöhessen létre.
+          </p>
+
+          <p>
+            <strong>Ez a kategória, csak nyáron jó idő esetén elérhető.</strong>
+          </p>
+        </SideboxrBanner>
+        <div className="container mx-auto px-4 py-8">
+          <p className="mb-9">
+            Noha a komfortzónából kiszakadás fontos pontja a fejlődésnek, mégis
+            ezt a kategóriát azoknak tudom ajánlani, akik biztonságban érzik
+            magukat a víz közelségében.
+          </p>
+          <p>
+            Kombináld a kategóriákat! Emellé a kategóra mellé érdemes egy
+            beltéri opciót is választani, hogy rossz idő esetén is meg tudjuk
+            tartani két hetenként az üléseket.
+          </p>
+        </div>
       </main>
       <footer></footer>
     </div>
