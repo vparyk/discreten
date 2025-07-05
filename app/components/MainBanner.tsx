8a8cbb8b18215a0a22fbda442b747e49401452f9
Venczel Patrik <v.papatrik@gmail.com>
2025-07-05 18:01:27 +0200

    fix: styling in rotated and low height resolution

--- app/components/MainBanner.tsx
+++ app/components/MainBanner.tsx
@@ -52,11 +52,11 @@ export default function MainBanner() {
   }, []);
 
   return (
-    <div className="h-screen flex flex-col lg:flex-row relative overflow-hidden ">
-      <div className="relative h-2/3 lg:h-screen  w-full flex  group flex items-end justify-end group">
+    <div className="min-h-screen lg:h-screen flex flex-col lg:flex-row relative overflow-hidden">
+      <div className="relative flex-1 min-h-[400px] flex group items-end justify-end">
         {/* Első kép */}
         <div
-          className="absolute inset-0 bg-cover bg-center transition-opacity duration-700 z-0"
+          className="absolute inset-0 bg-cover transition-opacity duration-700 z-0 bg-[position:23%_center] lg:bg-left"
           style={{
             backgroundImage: "url(/img/reten.webp)",
           }}
@@ -64,7 +64,7 @@ export default function MainBanner() {
 
         {/* Második kép */}
         <div
-          className="absolute inset-0 bg-cover bg-center transition-opacity duration-700 z-0"
+          className="absolute inset-0 bg-cover bg-[position:23%_center] lg:bg-left bg-left transition-opacity duration-700 z-0"
           style={{
             backgroundImage: "url(/img/reten-nincs-korlat.webp)",
             opacity: 1 - scrollOpacity,
@@ -72,8 +72,8 @@ export default function MainBanner() {
         ></div>
 
         {/* Tartalom */}
-        <div className="relative  bg-black/60 lg:bg-neutral-800/70 text-white p-6 shadow-lg text-center mb-15 z-20 rounded-bl-3xl">
-          <h3 className="lg:hidden text-lg lg:text-2xl text-right italic">
+        <div className="relative lg:hidden bg-black/60 lg:bg-neutral-800/70 text-white p-6 shadow-lg text-center mb-15 z-20 rounded-bl-3xl">
+          <h3 className="text-lg lg:text-2xl text-right italic font-semibold">
             Kreatív coaching eszközökkel
             <br /> a korlátokon túlra...
           </h3>
@@ -96,41 +96,39 @@ export default function MainBanner() {
         </div>
       </div>
 
-      <div className="h-1/3 lg:h-screen flex flex-col lg:flex-col">
-        {/* Mobil szöveges blokk a kép alatt */}
-        <div className="h-full lg:hidden bg-white py-6 pb-8 px-4 text-center">
-          <h1 className="text-xl font-bold text-gray-900 leading-tight lg:hidden">
-            Life és Business <br /> Coaching
-          </h1>
-          <p className="mt-3 text-gray-700">
-            Játék, mozgás, művészet alapú <br />
-            eszközhasználattal
-          </p>
-        </div>
+      {/* Mobil szöveges blokk a kép alatt */}
+      <div className="bg-white py-6 pb-8 px-4 text-center lg:hidden">
+        <h1 className="text-xl font-bold text-gray-900 leading-tight">
+          Life és Business <br /> Coaching
+        </h1>
+        <p className="mt-3 text-gray-700">
+          Játék, mozgás, művészet alapú <br />
+          eszközhasználattal
+        </p>
+      </div>
 
-        {/* Szövegdoboz – jobbra desktopon, alul mobilon */}
-        <div className="w-full h-full bg-rosemary p-10 flex flex-col items-center text-white justify-center 2xl:justify-start lg:border-l-2 relative xl:w-[300px] 2xl:w-[320px]">
-          <div className="flex flex-col flex-1 justify-center">
-            <h3 className="hidden lg:block text-lg lg:text-2xl text-right italic">
-              Kreatív coaching eszközökkel
-              <br /> a korlátokon túlra...
-            </h3>
-          </div>
-          <div className="space-y-4 text-base leading-relaxed max-w-lg flex flex-col gap-6 m-4 text-center font-semibold uppercase">
-            Kategóriák
-          </div>
-          {/* Lefelé mutató fehér kacsacsőr ikon */}
-          <button
-            className={`absolute left-1/2 -translate-x-1/2 bottom-4 flex flex-col items-center group transition-transform duration-300 ${
-              scrollOpacity === 0 ? "translate-y-4" : ""
-            }`}
-            aria-label="Görgess lejjebb"
-            onClick={handleScrollButtonClick}
-            type="button"
-          >
-            <DownArrowIcon />
-          </button>
+      {/* Szövegdoboz – jobbra desktopon, alul mobilon */}
+      <div className="lg:min-h-screen bg-rosemary p-10 pt-6 flex flex-col items-center text-white justify-center 2xl:justify-start lg:border-l-2 relative xl:w-[300px] 2xl:w-[320px]">
+        <div className="flex flex-col flex-1 justify-center">
+          <h3 className="hidden lg:block text-lg lg:text-2xl text-right italic">
+            Kreatív coaching eszközökkel
+            <br /> a korlátokon túlra...
+          </h3>
         </div>
+        <div className="space-y-4 text-base leading-relaxed max-w-lg flex flex-col gap-6 m-4 text-center font-semibold uppercase">
+          Kategóriák
+        </div>
+        {/* Lefelé mutató fehér kacsacsőr ikon */}
+        <button
+          className={`absolute left-1/2 -translate-x-1/2 bottom-4 flex flex-col items-center group transition-transform duration-300 ${
+            scrollOpacity === 0 ? "translate-y-3" : ""
+          }`}
+          aria-label="Görgess lejjebb"
+          onClick={handleScrollButtonClick}
+          type="button"
+        >
+          <DownArrowIcon />
+        </button>
       </div>
     </div>
   );
